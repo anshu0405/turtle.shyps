ebd052da64eda09b1e306f66b190c2a66fda1c48
anshu0405 <84734491+anshu0405@users.noreply.github.com>
2021-06-10 18:55:46 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,4 @@
+import turtle
+turtle.bacoloer("blue")
+cir = turtle.Turtle()
+cir,turtle.circle(100)
---
+++ tringle.py
@@ -0,0 +1,6 @@
+import turtle
+tringle = turtle.Turtle()
+for i in range(3):
+    tringle.forward(100)
+    tringle.left(120)
+turtle.done()
